4bf4f1dd2191bfe8e8fae1ea971064c7c1a69ba4
Fahad-G200 <fah_08@icloud.com>
2026-03-02 11:42:47 +0100

    Initial Commit

---
+++ HelpdeskGuard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelpdeskGuard
+//
+//  Created by Fahad Adnan Ashraf on 02/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelpdeskGuard/HelpdeskGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpdeskGuardApp.swift
+//  HelpdeskGuard
+//
+//  Created by Fahad Adnan Ashraf on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct HelpdeskGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
